5de446b2260646f09d7cbf0d0110a97b58a3552c
Arthur-Manzatto <arthur.manzatto2805@gmail.com>
2025-11-10 11:44:08 -0300

    function mediana() is working

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
 function calcular(){
+
+    let labelMedia = document.getElementById("media");
+    let labelMediana = document.getElementById("mediana");
+    let labelModa = document.getElementById("moda");
+
     let input = document.getElementById("numbers").value.toString().replaceAll(' ','');
     
-    let numeros = input.split(",");
+    let numeros = input.split(",").map(n => parseFloat(n));
 
-    media(numeros);
-    moda(numeros);
-    mediana(numeros);
+    labelMedia.innerText = media(numeros);
+    labelMediana.innerText = mediana(numeros); 
+    labelModa.innerText = moda(numeros);
     
 
 }
 
 function media(numbers){
-    let labelMedia = document.getElementById("media");
+    
     let fTotal = 0;
     let iDivisor = 0;
     let fMedia = 0;
 
     numbers.forEach(n => {
-        n = parseInt(n);
         fTotal += n;
     });
     
     iDivisor = numbers.length;
     fMedia = fTotal/iDivisor;
 
-    labelMedia.innerText += fMedia;
+    return "Média: " + fMedia;
     
 }
-function moda(numbers){
+function mediana(numbers){
+    numbers.sort((a, b) => a - b);
+    let count = numbers.length;
+
+    let iMeio = Math.floor(count / 2); // Pega o índice do meio
+
+    console.log("INDEX DO MEIO: " + iMeio);
+    console.log("VALOR DO MEIO: " + numbers[iMeio]);
+
+    if (count % 2 === 0) { 
+
+        let middle1 = numbers[iMeio - 1];
+        let middle2 = numbers[iMeio];
+        return "Mediana: " + ((middle1 + middle2) / 2);
+    } else {
+
+        return "Mediana: " + numbers[iMeio];
+    }
+
 
 }
-function mediana(numbers){
-
+function moda(numbers){
+    
 }
